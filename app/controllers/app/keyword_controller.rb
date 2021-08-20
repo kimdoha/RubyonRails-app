fab7d9925909104d9229b4998f2d68420c46f795
minnji88 <kkhhjj3393@gmail.com>
2021-08-20 11:05:12 +0900

    키워드 추가 및 삭제

--- app/controllers/app/keyword_controller.rb
+++ app/controllers/app/keyword_controller.rb
@@ -1,8 +1,57 @@
 module App
     class KeywordController < ApplicationController
+
+        # @Get http://localhost:3000/app/keyword
+        # 키워드 전체 조회
         def index
             keywords = Keyword.select("keywordId, userId, keyword").order('createAt DESC')
-            render json: { isSuccess:true, code:1000, message:"내 키워드 전체조회 완료", result: keywords }, status: :ok
+            render json: { 
+                isSuccess:true, 
+                code:1000, 
+                message:"내 키워드 전체조회 완료", 
+                result: keywords }, status: :ok
+
+        end
+
+        
+        # @Post http://localhost:3000/app/keyword
+        # 키워드 생성
+        def create
+            keyword = Keyword.new(keyword_params)
+
+            if keyword.save
+                render json: { 
+                    isSuccess:true, 
+                    code:1000, 
+                    message:"키워드 생성 완료", 
+                    result: keyword }, status: :ok
+            else
+                render json: { 
+                    isSuccess:false, 
+                    code:3001, 
+                    message:"키워드 생성 실패", 
+                    result: keyword.errors }, 
+                status: :unprocessable_entity
+            end
+        end
+
+
+        # @DELETE http://localhost:3000/app/keyword
+        # 키워드 삭제
+        def destroy
+            keyword = Keyword.find(params[:id])
+            keyword.destroy
+            render json: { 
+                isSuccess:true, 
+                code:1000, 
+                message:"키워드 삭제 완료", 
+                result: keyword }, status: :ok
+        end
+
+
+        private
+        def keyword_params
+            params.permit(:keyword)
         end
 
     end
